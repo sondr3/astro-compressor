ccae513b209ed0b4d364031b1fc0031e0a756b89
Sondre Aasemoen <sondre@eons.io>
2022-12-29 12:16:54 +0100

    Remove globby dependency, use stdlib

--- src/compress.ts
+++ src/compress.ts
@@ -1,11 +1,23 @@
 import { createReadStream, createWriteStream } from "node:fs";
+import { readdir } from "node:fs/promises";
+import { resolve } from "node:path";
 import { hrtime } from "node:process";
 import { promises as stream } from "node:stream";
 import { createBrotliCompress, createGzip } from "node:zlib";
 
-import { globby } from "globby";
-
 import { Logger } from "./logger.js";
+
+async function* walkDir(dir: string): AsyncGenerator<string> {
+  const entries = await readdir(dir, { withFileTypes: true });
+  for (const entry of entries) {
+    const name = resolve(dir, entry.name);
+    if (entry.isDirectory()) {
+      yield* walkDir(name);
+    } else if (filterFile(entry.name)) {
+      yield name;
+    }
+  }
+}
 
 const filterFile = (file: string): boolean => {
   return [".css", ".js", ".html", ".xml", ".cjs", ".mjs", ".svg", ".txt"].some((ext) =>
@@ -16,8 +28,9 @@ const filterFile = (file: string): boolean => {
 export const gzip = async (dir: URL): Promise<void> => {
   const start = hrtime.bigint();
 
-  const files = (await globby(`${dir.pathname}/**/*`)).filter(filterFile);
-  for (const file of files) {
+  let counter = 0;
+  for await (const file of walkDir(dir.pathname)) {
+    counter += 1;
     const source = createReadStream(file);
     const destination = createWriteStream(`${file}.gz`);
     const gzip = createGzip({ level: 9 });
@@ -25,14 +38,15 @@ export const gzip = async (dir: URL): Promise<void> => {
   }
 
   const end = hrtime.bigint();
-  Logger.success(`finished gzip of ${files.length} files in ${(end - start) / 1000000n}m`);
+  Logger.success(`finished gzip of ${counter} files in ${(end - start) / 1000000n}m`);
 };
 
 export const brotli = async (dir: URL): Promise<void> => {
   const start = hrtime.bigint();
 
-  const files = (await globby(`${dir.pathname}/**/*`)).filter(filterFile);
-  for (const file of files) {
+  let counter = 0;
+  for await (const file of walkDir(dir.pathname)) {
+    counter += 1;
     const source = createReadStream(file);
     const destination = createWriteStream(`${file}.br`);
     const brotli = createBrotliCompress();
@@ -40,5 +54,5 @@ export const brotli = async (dir: URL): Promise<void> => {
   }
 
   const end = hrtime.bigint();
-  Logger.success(`finished brotli of ${files.length} files in ${(end - start) / 1000000n}m`);
+  Logger.success(`finished brotli of ${counter} files in ${(end - start) / 1000000n}m`);
 };
