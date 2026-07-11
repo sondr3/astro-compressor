681277df007d802a34b5a35e511b0c6254916890
Sondre Aasemoen <sondre@eons.io>
2026-07-11 16:37:23 +0200

    Add astro check to test build

--- test/astro.config.mjs
+++ test/astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from "astro/config"
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [compressor({ gzip: true, brotli: { chunkSize: 16 * 1024 } })],
+	integrations: [compressor({ gzip: true, brotli: { chunkSize: 16 * 1024 }, batchSize: 12 })],
 })
