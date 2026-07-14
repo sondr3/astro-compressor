99145712ab65383427b50f04311fe312e8c39091
Sondre Aasemoen <sondre@eons.io>
2026-07-14 19:34:42 +0200

    Rename and refactor hooks

--- integration-tests/hooks/astro.config.mjs
+++ integration-tests/hooks/astro.config.mjs
@@ -6,11 +6,11 @@ export default defineConfig({
 	integrations: [
 		compressor({
 			hooks: {
-				"compressor:file:before": ({ filePath, format }) => {
+				preCompression: ({ filePath, format }) => {
 					if (filePath.endsWith("xml") && format === "zstd") return "skip"
 					return "keep"
 				},
-				"compressor:file:after": ({ outputPath, format }) => {
+				postCompression: ({ outputPath, format }) => {
 					if (outputPath.includes("sitemap.xml") && format === "gzip") return "skip"
 					return "keep"
 				},
